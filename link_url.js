f48fd60ec0f5df8ba4eee7ac2c678a99df380b7c
likeweb@likeweb.co.kr <likeweb@likeweb.co.kr>
2024-07-19 14:27:49 +0900

    수정

--- link_url.js
+++ link_url.js
@@ -2,13 +2,23 @@ const shome = () => location.href="index.html";
 const menu1 = () => location.href="info1.html";
 const menu11 = () => location.href="info1.html";
 const menu12 = () => location.href="info2.html";
-const menu13 = () => location.href="info3.html";
-const menu2 = () => location.href="program1.html";
-const menu21 = () => location.href="program1.html";
-const menu22 = () => location.href="program2.html";
-const menu23 = () => location.href="program3.html";
-const menu24 = () => location.href="program4.html";
-const menu25 = () => location.href="program5.html";
+// const menu13 = () => location.href="info3.html";
+// const menu2 = () => location.href="program1.html";
+// const menu21 = () => location.href="program1.html";
+// const menu22 = () => location.href="program2.html";
+// const menu23 = () => location.href="program3.html";
+// const menu24 = () => location.href="program4.html";
+// const menu25 = () => location.href="program5.html";
+
+const menu13 = () => alert("페이지 준비 중입니다");
+const menu2 = () => alert("페이지 준비 중입니다");
+const menu21 = () => alert("페이지 준비 중입니다");
+const menu22 = () => alert("페이지 준비 중입니다");
+const menu23 = () => alert("페이지 준비 중입니다");
+const menu24 = () => alert("페이지 준비 중입니다");
+const menu25 = () => alert("페이지 준비 중입니다");
+
+
 const menu3 = () => location.href="notice1.html";
 const menu31 = () => location.href="notice1.html";
 const menu32= () => location.href="faq1.html";
