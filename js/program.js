4e0e892b0fb0ad0ff20e939b8a4b6f5d896fa348
sol772 <53934919+sol772@users.noreply.github.com>
2024-08-08 17:41:14 +0900

    프로그램 모바일 스와이프 추가

--- js/program.js
+++ js/program.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    // FAQ
+    // program_ul
     const programMenu = document.querySelectorAll('.program_ul .tit_box button');
     programMenu.forEach(el => {
         el.addEventListener('click', (e) => {
@@ -11,5 +11,46 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
+    //swipe_box
+    var swipeBoxes = document.querySelectorAll('.swipe_box');
 
+    swipeBoxes.forEach(function(swipeBox) {
+        // 스크롤 이벤트 리스너 추가
+        swipeBox.addEventListener('scroll', function() {
+            var swipeImg = swipeBox.querySelector('.swipe_img');
+            if (swipeImg) {
+                fadeOut(swipeImg, 100);
+            }
+        });
+
+        // 터치 이동 이벤트 리스너 추가
+        swipeBox.addEventListener('touchmove', function() {
+            var swipeImg = swipeBox.querySelector('.swipe_img');
+            if (swipeImg) {
+                fadeOut(swipeImg, 100);
+            }
+        });
+    });
+
+
+    // fadeOut 함수
+    function fadeOut(element, duration) {
+        var opacity = 1;
+        var interval = 50;
+        var gap = interval / duration;
+
+        function fade() {
+            opacity -= gap;
+            if (opacity <= 0) {
+                opacity = 0;
+                element.style.display = 'none';
+            }
+            element.style.opacity = opacity;
+            if (opacity > 0) {
+                setTimeout(fade, interval);
+            }
+        }
+
+        fade();
+    }
 });
