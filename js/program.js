e844c102823820c9926df2ce7705d4a6c120f5ff
likeweb@likeweb.co.kr <likeweb@likeweb.co.kr>
2024-08-12 21:14:21 +0900

    새로운 사이트 커밋

---
+++ js/program.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // program_ul
+  const programMenu = document.querySelectorAll(".program_ul .tit_box");
+  programMenu.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      let liEl = e.target.closest("li");
+      liEl.classList.toggle("on");
+    });
+  });
+
+  //swipe_box
+  var swipeBoxes = document.querySelectorAll(".swipe_box");
+
+  swipeBoxes.forEach(function (swipeBox) {
+    // 스크롤 이벤트 리스너 추가
+    swipeBox.addEventListener("scroll", function () {
+      var swipeImg = swipeBox.querySelector(".swipe_img");
+      if (swipeImg && swipeImg.style.opacity !== "0") {
+        fadeOut(swipeImg, 100);
+      }
+    });
+
+    // 터치 이동 이벤트 리스너 추가
+    swipeBox.addEventListener("touchmove", function () {
+      var swipeImg = swipeBox.querySelector(".swipe_img");
+      if (swipeImg && swipeImg.style.opacity !== "0") {
+        fadeOut(swipeImg, 100);
+      }
+    });
+
+    // 클릭 이벤트 리스너 추가
+    swipeBox.addEventListener("click", function () {
+      var swipeImg = swipeBox.querySelector(".swipe_img");
+      if (swipeImg && swipeImg.style.opacity !== "0") {
+        fadeOut(swipeImg, 100);
+      }
+    });
+  });
+
+  // fadeOut 함수
+  function fadeOut(element, duration) {
+    var opacity = 1;
+    var interval = 50;
+    var gap = interval / duration;
+
+    function fade() {
+      opacity -= gap;
+      if (opacity <= 0) {
+        opacity = 0;
+        element.style.opacity = opacity;
+        element.style.display = "none";
+      } else {
+        element.style.opacity = opacity;
+        setTimeout(fade, interval);
+      }
+    }
+
+    fade();
+  }
+
+  let tabletMobileTable = document.querySelectorAll(".table_tablet_mobile");
+  tabletMobileTable.forEach(function (table) {
+    table.addEventListener("click", function () {
+      const dialog = this.nextElementSibling;
+      dialog.showModal();
+    });
+  });
+
+  let dialogCloseButton = document.querySelectorAll(".table_popup button");
+  dialogCloseButton.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const dialog = this.closest("dialog");
+      dialog.close();
+    });
+  });
+});
